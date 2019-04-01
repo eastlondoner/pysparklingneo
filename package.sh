6ca3e5e3865aa99a227b30ef447f149442d7612d
Andrew Jefferson <andrew.jefferson@neotechnology.com>
2019-04-01 20:10:40 +0200

    build pyspark_neo jar in docker

--- package.sh
+++ package.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-pushd pyspark-neo4j
-  make dist
-popd
+docker run --rm -v "${HOME}/.m2":/root/.m2 -v "${HOME}/.ivy2":/root/.ivy2 -v "$(pwd)/pyspark-neo4j":/pyspark-neo4j --workdir /pyspark-neo4j hseeberger/scala-sbt bash -c "apt-get install -y make zip && make dist"
